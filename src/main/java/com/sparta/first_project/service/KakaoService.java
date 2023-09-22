619962bef8b2081d8be1cc50b8930cb1794e4e42
hyeongchul13 <jc_22@naver.com>
2023-09-22 13:52:16 +0900

    카카오 로그인 구현 및 토큰 발행

--- src/main/java/com/sparta/first_project/service/KakaoService.java
+++ src/main/java/com/sparta/first_project/service/KakaoService.java
@@ -67,7 +67,7 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "6a3be49e8029aa136c5a1d8c692a2f84");
-        body.add("redirect_uri", "http://localhost:8080/api/user/kakao/callback");
+        body.add("redirect_uri", "http://localhost:5173/api/user/kakao/callback");
         body.add("code", code);
 
         RequestEntity<MultiValueMap<String, String>> requestEntity = RequestEntity
