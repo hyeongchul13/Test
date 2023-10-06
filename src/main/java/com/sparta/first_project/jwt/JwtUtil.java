e44c6f87d97ab4a6bb46371be3a408c909d065e5
hyeongchul13 <jc_22@naver.com>
2023-10-06 23:35:15 +0900

    #3 카카오 로그인 구현

--- src/main/java/com/sparta/first_project/jwt/JwtUtil.java
+++ src/main/java/com/sparta/first_project/jwt/JwtUtil.java
@@ -25,7 +25,7 @@ public class JwtUtil {
     // Token 식별자
     public static final String BEARER_PREFIX = "Bearer ";
     // 토큰 만료시간
-    private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
+    private final long TOKEN_TIME = 1 * 1 * 1000L; // 60분
 
     private final long RefreshTOKEN_TIME = 7 * 24 * 60 * 60 * 1000L;
 
